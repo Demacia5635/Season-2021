a7b7554a27d71e0d4bb8835294243e7d0560d518
Noam Alffasy <7583207+noamalffasy@users.noreply.github.com>
2021-01-24 15:36:36 +0200

    Added a chassis with bad code for testing's sake

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -45,4 +45,34 @@ public final class Constants {
     // Ramsete Parameters (in meters and seconds)
     public static final double RAMSETE_B = 2;
     public static final double RAMSETE_ZETA = 0.7;
+
+    /**
+     * Yarmus Constants
+     */
+
+    // Ports
+    public static final int BackRightID = 1;
+    public static final int BackLeftID = 4;
+    public static final int FrontRightID = 2;
+    public static final int FrontLeftID = 3;
+    public static final int GyroID = 3;
+
+    // Wheel shit
+    public static final double CheesyPuffsMeter = 2.54 / 100.;
+    public static final int wheelD = 6; /// in inches
+    public static final double D = wheelD * CheesyPuffsMeter; // in meters
+
+    // Pulses
+    public static final double P = D * Math.PI; // wheel paremeter
+    public static final double pulses = 800;
+    public static final double pulsesToMeter = (1 / Constants.P) * Constants.pulses;
+    public static final double pulseInMeter = P / pulses; 
+
+    // Characteristics
+    public static final double kp = 0.0001;
+    public static final double kd = 0;
+    public static final double ki = 0;
+    public static final double ks = 0.797;
+    public static final double kv = 2.34;
+    public static final double ka = 0.862;
 }
--- src/main/java/frc/robot/subsystems/Chassis.java
+++ src/main/java/frc/robot/subsystems/Chassis.java
@@ -7,18 +7,49 @@
 
 package frc.robot.subsystems;
 
+import frc.robot.Constants;
+
+import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.DemandType;
+import com.ctre.phoenix.motorcontrol.can.TalonSRX;
+import com.ctre.phoenix.sensors.PigeonIMU;
+
 import edu.wpi.first.wpilibj.geometry.Pose2d;
+import edu.wpi.first.wpilibj.geometry.Rotation2d;
+import edu.wpi.first.wpilibj.kinematics.DifferentialDriveOdometry;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class Chassis extends SubsystemBase {
+  private final TalonSRX BackLeft = new TalonSRX(Constants.BackLeftID);
+  private final TalonSRX BackRight = new TalonSRX(Constants.BackRightID);
+  private final TalonSRX FrontLeft = new TalonSRX(Constants.FrontLeftID);
+  private final TalonSRX FrontRight = new TalonSRX(Constants.FrontRightID);
+  private final PigeonIMU gyro = new PigeonIMU(Constants.GyroID);
+  private final DifferentialDriveOdometry odometry;
+
+  private double baseAngle;
+
   /**
    * Creates a new Chassis.
    */
   public Chassis() {
+    BackRight.follow(FrontRight);
+    FrontLeft.follow(BackLeft);
+
+    BackLeft.config_kP(0, Constants.kp);
+    FrontLeft.config_kP(0, Constants.kp);
+    FrontRight.config_kP(0, Constants.kp);
+    BackRight.config_kP(0, Constants.kp);
+
+    resetEncoders();
+    odometry = new DifferentialDriveOdometry(Rotation2d.fromDegrees(getAngle()));
+
+    baseAngle = 0;
   }
 
   @Override
   public void periodic() {
+    odometry.update(Rotation2d.fromDegrees(getAngle()), getLeftDistance(), getRightDistance());
   }
 
   /**
@@ -27,7 +58,19 @@ public class Chassis extends SubsystemBase {
    * @return The pose.
    */
   public Pose2d getPose() {
-    return new Pose2d();
+    return odometry.getPoseMeters();
+  }
+
+  public double getAngle() {
+    return gyro.getFusedHeading() + baseAngle;
+  }
+
+  public double getLeftDistance() {
+    return BackLeft.getSelectedSensorPosition() / Constants.pulseInMeter;
+  }
+
+  public double getRightDistance() {
+    return FrontRight.getSelectedSensorPosition() / Constants.pulseInMeter;
   }
 
   /**
@@ -36,14 +79,46 @@ public class Chassis extends SubsystemBase {
    * @param pose The pose to which to set the odometry.
    */
   public void resetOdometry(Pose2d pose) {
+    resetEncoders();
+    resetGyro(pose.getRotation().getDegrees());
+    odometry.resetPosition(pose, pose.getRotation());
+  }
+
+  public void resetGyro() {
+    gyro.setFusedHeading(0);
+  }
+
+  public void resetGyro(double angle) {
+    baseAngle = angle - getAngle();
+  }
+
+  public void resetEncoders() {
+    BackLeft.setSelectedSensorPosition(0);
+    FrontRight.setSelectedSensorPosition(0);
   }
 
   /**
    * Controls the left and right sides of the drive directly with voltages.
    *
-   * @param leftVolts  the commanded left output
-   * @param rightVolts the commanded right output
+   * @param leftVelocity  the commanded left velocity
+   * @param rightVelocity the commanded right velocity
    */
-  public void setVelocity(double leftVolts, double rightVolts) {
+  public void setVelocity(double leftVelocity, double rightVelocity) {
+    double leftVelPulse = leftVelocity / Constants.pulseInMeter / 10;
+    double rightVelPulse = rightVelocity / Constants.pulseInMeter / 10;
+
+    if (leftVelocity == 0) {
+      BackLeft.set(ControlMode.Velocity, 0, DemandType.ArbitraryFeedForward, 0);
+    } else {
+      BackLeft.set(ControlMode.Velocity, -leftVelPulse, DemandType.ArbitraryFeedForward,
+          (leftVelocity * Constants.ks + Constants.kv) / 12);
+    }
+
+    if (rightVelocity == 0) {
+      FrontRight.set(ControlMode.Velocity, 0, DemandType.ArbitraryFeedForward, 0);
+    } else {
+      FrontRight.set(ControlMode.Velocity, -rightVelPulse, DemandType.ArbitraryFeedForward,
+          (rightVelocity * Constants.ks + Constants.kv) / 12);
+    }
   }
 }
